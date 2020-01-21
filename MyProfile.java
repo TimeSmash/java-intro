87f02d06d6fcc94c421b32bae800e323fdf4fda8
TimeSmash <mcumm64@gmail.com>
2020-01-20 23:16:16 -0500

    Added file MyProfile showing utilization of various datatypes, then compiled with javac into class

---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+        String name = "Matt";
+        int age = 99;
+        double desiredSalary = 999999999.99;
+        char gender = 'M';
+        boolean lookingForJob = true;
+	}
+}
