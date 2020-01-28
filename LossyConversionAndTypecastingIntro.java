91d93150ccdf86d9d1706e209e19ed8f25ddacad
TimeSmash <mcumm64@gmail.com>
2020-01-27 23:22:37 -0500

    Added Modulo java+class, goes over modulo operator. Also started another file about lossy conversion + typecasting.

---
+++ LossyConversionAndTypecastingIntro.java
@@ -0,0 +1,7 @@
+public class LossyConversionAndTypecastingIntro{
+    public static void main(String[] args) {   
+		int number = 8;
+        int a = (int) 176.5+number;
+        System.out.println(a);
+	}
+}
