95f2e6763f5a776bf99fd7d3b2db91de8eba5196
TimeSmash <mcumm64@gmail.com>
2020-01-27 22:25:30 -0500

    Added Modulo java+class, shows how Modulo operator works.

---
+++ Modulo.java
@@ -0,0 +1,8 @@
+public class Modulo {
+    //Modulo works similar as it does in JS. Gets the remainder of a number
+	public static void main(String[] args) {   
+		int students = 26;
+    int remainder = 26 % 3;
+    System.out.println(remainder);
+	}
+}
