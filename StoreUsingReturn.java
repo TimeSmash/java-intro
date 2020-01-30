a740a5a37b1eaf6756ad5ce3c6d9768d04219cab
TimeSmash <mcumm64@gmail.com>
2020-01-30 01:35:45 -0500

    Added StoreUsingReturn, explores methods and how return keyword works.

---
+++ StoreUsingReturn.java
@@ -0,0 +1,29 @@
+public class StoreUsingReturn {
+    // instance fields
+    String productType;
+    double price;
+    
+    // constructor method
+    public StoreUsingReturn(String product, double initialPrice) {
+      productType = product;
+      price = initialPrice;
+    }
+    
+    // increase price method
+    public void increasePrice(double priceToAdd){
+      double newPrice = price + priceToAdd;
+      price = newPrice;
+    }
+    
+    // get price with tax method
+    public double getPriceWithTax(){
+      double totalPrice = price + price*0.08;
+      return totalPrice;
+    }
+    // main method
+    public static void main(String[] args) {
+      StoreUsingReturn lemonadeStand = new StoreUsingReturn("Lemonade", 3.75);
+    double lemonadePrice = lemonadeStand.getPriceWithTax();
+      System.out.println(lemonadePrice);
+    }
+  }
